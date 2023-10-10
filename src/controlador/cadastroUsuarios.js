bf49ed3c0733c6e32bdd55463cec1181cb101e97
Lauane Gonçalves <lauane.gcsilva@gmail.com>
2023-10-10 17:08:00 -0300

    adicionando funcionalidades cadastro de usuarios

--- src/controlador/cadastroUsuarios.js
+++ src/controlador/cadastroUsuarios.js
@@ -1,3 +1,18 @@
 const cadastrarUsuario = async (req, res) => {
-  return res.json("certo");
+  const { nome, email, senha } = req.body;
+
+  try {
+    const usuarioEncontrado = await knex("usuarios").where({ email }).first();
+
+    if (usuarioEncontrado) {
+      return res.status(400).json({ mensagem: "O email já existe" });
+    }
+
+    return res.status(201).json(usuarioCadastrado);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ mensagem: "Erro interno do servidor" });
+  }
 };
+
+module.exports = cadastrarUsuario;
