fbe76b6f7ef1d5dd10d683c8a27d200e9301bff4
Gleicy Silva <glegleicy5@gmail.com>
2023-10-10 20:33:16 -0300

    Fix: corrigindo nome da variavel de ambiente de conexao do banco

--- src/conexao.js
+++ src/conexao.js
@@ -4,7 +4,7 @@ const knex = require('knex')({
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         user: process.env.DB_USER,
-        password: process.env.DB_HOST,
+        password: process.env.DB_PASS,
         database: process.env.DB_NAME,
         ssl: { rejectUnauthorized: false }
     }
