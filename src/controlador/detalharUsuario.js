58c5f938b9104e34000cea0a834493b7382c2290
Gleicy Silva <glegleicy5@gmail.com>
2023-10-11 20:30:16 -0300

    Refactor: Refatorado a logica do controlador

--- src/controlador/detalharUsuario.js
+++ src/controlador/detalharUsuario.js
@@ -1,23 +1,5 @@
-const bcrypt = require("bcrypt");
-const knex = require("../conexao");
-
 const detalharUsuario = async (req, res) => {
-    const userId = req.userId;
-
-    try {
-        const usuario = await knex("usuarios")
-            .select("id", "nome", "email")
-            .where("id", userId)
-            .first();
-
-        if (!usuario) {
-            return res.status(404).json({ mensagem: "Usuário não encontrado." });
-        }
-
-        return res.status(200).json(usuario);
-    } catch (error) {
-        return res.status(500).json({ mensagem: "Erro ao buscar o usuário." });
-    }
-};
+    return res.status(200).json(req.usuario);
+}
 
 module.exports = detalharUsuario;
