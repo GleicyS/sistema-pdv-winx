63d396731f724ae6f86141350db26f561394def6
Gleicy Silva <glegleicy5@gmail.com>
2023-10-18 18:53:49 -0300

    Feat: Adicionando novas tabelas no banco de dados

--- src/dump.sql
+++ src/dump.sql
@@ -22,3 +22,27 @@ VALUES
 ('Bebê'),
 ('Games');
 
+CREATE TABLE clientes (
+  id SERIAL PRIMARY KEY,
+  nome VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  cpf VARCHAR(11) NOT NULL UNIQUE,
+  cep VARCHAR(8),
+  rua VARCHAR(255),
+  numero VARCHAR(20),
+  bairro VARCHAR(100),
+  cidade VARCHAR(100),
+  estado VARCHAR(2)
+);
+
+CREATE TABLE produtos (
+	id SERIAL PRIMARY KEY,
+  descricao VARCHAR(255) NOT NULL,
+  quantidade_estoque INTEGER NOT NULL,
+  valor INTEGER NOT NULL,
+  categoria_id INTEGER REFERENCES categorias(id)
+);
+
+
+
+
