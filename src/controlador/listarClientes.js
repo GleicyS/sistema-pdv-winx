e7ccb91bfa516199d1479d19e344ce12ca63cc4d
Gleicy Silva <glegleicy5@gmail.com>
2023-10-19 20:31:22 -0300

    Refactor: Modificando query de busca

--- src/controlador/listarClientes.js
+++ src/controlador/listarClientes.js
@@ -2,7 +2,10 @@ const knex = require("../conexao");
 
 const listarClientes = async (req, res) => {
   try {
-    const clientes = await knex("clientes");
+    const clientes = await knex('clientes').select('nome', 'email', 'cpf', knex.raw(`COALESCE(cep, 'Não informado') as cep, 
+    COALESCE(rua, 'Não informado') as rua,
+   COALESCE(numero, 'Não informado') as numero, COALESCE(bairro, 'Não informado') as bairro, COALESCE(cidade, 'Não informado') as cidade, 
+   COALESCE(estado, 'Não informado') as estado`))
     return res.json(clientes);
   } catch (error) {
     return res.status(500).json({ mensagem: "Erro interno do servidor" });
@@ -10,3 +13,4 @@ const listarClientes = async (req, res) => {
 };
 
 module.exports = listarClientes;
+
