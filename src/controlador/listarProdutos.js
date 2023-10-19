f1f66c6e989387618e5d224ef5bebd5e97e8373b
Gleicy Silva <glegleicy5@gmail.com>
2023-10-19 17:37:37 -0300

    Refactor: Atualizando logica

--- src/controlador/listarProdutos.js
+++ src/controlador/listarProdutos.js
@@ -2,17 +2,20 @@ const knex = require("../conexao");
 
 const listarProdutos = async (req, res) => {
     const { categoria_id } = req.query;
-
     try {
-        const produtos = await knex('produtos').where((query) => {
-            if (categoria_id) {
-                return query.where('categoria_id', 'ilike', `%${categoria_id}%`);
-            } else {
-                return query;
-            }
-        });
+        let produtos;
 
-        return res.json(produtos);
+        if (categoria_id) {
+            produtos = await knex('produtos').where({ categoria_id: categoria_id })
+        } else {
+            produtos = await knex('produtos')
+        }
+
+        if (produtos.length === 0) {
+            return res.status(404).json({ mensagem: "Nenhum produto encontrado." })
+        } else {
+            return res.status(200).json(produtos)
+        }
 
     } catch (error) {
         return res.status(500).json({ mensagem: "Erro interno do servidor" });
