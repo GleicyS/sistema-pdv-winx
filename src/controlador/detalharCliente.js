ff17a85f49506e65cab3e72a8701ee3af4c4781f
Gleicy Silva <glegleicy5@gmail.com>
2023-10-19 20:31:52 -0300

    Refactor: Modificando query de busca

--- src/controlador/detalharCliente.js
+++ src/controlador/detalharCliente.js
@@ -5,7 +5,10 @@ const detalharCliente = async (req, res) => {
     const { id } = req.params
 
     try {
-        const clienteEncontrado = await knex('clientes').where({ id }).first()
+        const clienteEncontrado = await knex('clientes').select('nome', 'email', 'cpf', knex.raw(`COALESCE(cep, 'Não informado') as cep, 
+         COALESCE(rua, 'Não informado') as rua,
+        COALESCE(numero, 'Não informado') as numero, COALESCE(bairro, 'Não informado') as bairro, COALESCE(cidade, 'Não informado') as cidade, 
+        COALESCE(estado, 'Não informado') as estado`)).where({ id }).first()
 
         if (!clienteEncontrado) {
             return res.status(404).json({ mensagem: 'Cliente não encontrado' })
