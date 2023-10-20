e3539615273b9085c334064ab54855e0b37a2f0c
Gleicy Silva <glegleicy5@gmail.com>
2023-10-20 17:59:38 -0300

    Refactor: Mudando logica

--- src/controlador/produtos/listarProdutos.js
+++ src/controlador/produtos/listarProdutos.js
@@ -3,19 +3,14 @@ const knex = require("../../conexao");
 const listarProdutos = async (req, res) => {
     const { categoria_id } = req.query;
     try {
-        let produtos;
 
         if (categoria_id) {
-            produtos = await knex('produtos').where({ categoria_id: categoria_id })
+            const categorias = Array.isArray(categoria_id) ? categoria_id : [categoria_id]
+            produtos = await knex('produtos').whereIn('categoria_id', categorias)
         } else {
             produtos = await knex('produtos')
         }
-
-        if (produtos.length === 0) {
-            return res.status(404).json({ mensagem: "Nenhum produto encontrado." })
-        } else {
-            return res.status(200).json(produtos)
-        }
+        return res.status(200).json(produtos)
 
     } catch (error) {
         return res.status(500).json({ mensagem: "Erro interno do servidor" });
