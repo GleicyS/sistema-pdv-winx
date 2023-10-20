8c8647e3fb84bd429f6cd22537c89ba333feb404
Gleicy Silva <glegleicy5@gmail.com>
2023-10-20 17:54:48 -0300

    Refactor: Modificando validações

--- src/validacoes/produtoSchema.js
+++ src/validacoes/produtoSchema.js
@@ -1,21 +1,23 @@
 const joi = require('joi')
 
 const produtoSchema = joi.object({
-    descricao: joi.string().trim().required().messages({
+    descricao: joi.string().trim().required().pattern(/^[^0-9]*$/).messages({
         'any.required': 'O campo descrição é obrigatório',
+        'string.base': 'O campo descrição deve ser uma string',
         'string.empty': 'O campo descrição é obrigatório',
+        'string.pattern.base': 'O campo descrição não pode conter números',
     }),
     quantidade_estoque: joi.number().required().messages({
         'any.required': 'O campo quantidade em estoque é obrigatório',
-        'string.empty': 'O campo quantidade em estoque é obrigatório',
+        'number.base': 'O campo quantidade em estoque deve ser um número'
     }),
     valor: joi.number().required().messages({
         'any.required': 'O campo valor é obrigatório',
-        'string.empty': 'O campo valor é obrigatório'
+        'number.base': 'O campo valor deve ser um número'
     }),
     categoria_id: joi.number().required().messages({
         'any.required': 'O campo categoria é obrigatório',
-        'string.empty': 'O campo categoria é obrigatório'
+        'number.base': 'O campo categoria_id deve ser um número'
     })
 })
 
