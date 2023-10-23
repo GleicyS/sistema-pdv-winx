a940935e09bd3ab6cdfb83ec92178985b0289280
Lauane Gonçalves <lauane.gcsilva@gmail.com>
2023-10-23 20:52:56 -0300

    feat: adicionando funcionalidades no pedidoSchema

--- src/validacoes/pedidoSchema.js
+++ src/validacoes/pedidoSchema.js
@@ -1,29 +1,28 @@
 const joi = require("joi");
 
-const produtoSchema = joi.object({
-  descricao: joi
-    .string()
-    .trim()
-    .required()
-    .pattern(/^[^0-9]*$/)
-    .messages({
-      "any.required": "O campo descrição é obrigatório",
-      "string.base": "O campo descrição deve ser uma string",
-      "string.empty": "O campo descrição é obrigatório",
-      "string.pattern.base": "O campo descrição não pode conter números",
-    }),
-  quantidade_estoque: joi.number().required().messages({
-    "any.required": "O campo quantidade em estoque é obrigatório",
-    "number.base": "O campo quantidade em estoque deve ser um número",
+const pedidoSchema = joi.object({
+  cliente_id: joi.number().required().messages({
+    "any.required": "O campo categoria_id é obrigatório",
+    "number.base": "O campo categoria_id deve ser uma number",
   }),
-  valor: joi.number().required().messages({
-    "any.required": "O campo valor é obrigatório",
-    "number.base": "O campo valor deve ser um número",
+  observacao: joi.string().messages({
+    "string.empty": "O campo observação está vazio",
   }),
-  categoria_id: joi.number().required().messages({
-    "any.required": "O campo categoria é obrigatório",
-    "number.base": "O campo categoria_id deve ser um número",
-  }),
+  pedido_produtos: joi
+    .array()
+    .items(
+      joi.object({
+        produto_id: joi.number().required().messages({
+          "any.required": "O campo produto_id é obrigatório",
+          "number.base": "O campo produto_idd deve ser um número",
+        }),
+        quantidade_produto: joi.number().required().messages({
+          "any.required": "O campo quantidade_produto é obrigatório",
+          "number.base": "O campo quantidade_produto deve ser um número",
+        }),
+      })
+    )
+    .required(),
 });
 
-module.exports = produtoSchema;
+module.exports = pedidoSchema;
