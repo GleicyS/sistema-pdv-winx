f4024e68f11af3c26e2662c302cf25ab22e85825
Lauane Gonçalves <lauane.gcsilva@gmail.com>
2023-10-23 21:00:46 -0300

    feat: atualizando localização do knex

--- src/controlador/cadastrarPedido.js
+++ src/controlador/cadastrarPedido.js
@@ -1,4 +1,4 @@
-const knex = require("../../conexao");
+const knex = require("../conexao");
 
 const cadastrarPedido = async (req, res) => {
   const { cliente_id, observacao, pedido_produtos } = req.body;
