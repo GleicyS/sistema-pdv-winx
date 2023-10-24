f89d09736e7dc1afa6e6a9381a0514909bbc21b0
Lauane Gonçalves <lauane.gcsilva@gmail.com>
2023-10-23 22:37:31 -0300

    feat: validando de pedido_produtos é um array

--- src/controlador/pedidos/cadastrarPedido.js
+++ src/controlador/pedidos/cadastrarPedido.js
@@ -4,6 +4,12 @@ const cadastrarPedido = async (req, res) => {
   const { id } = req.usuario;
   const { cliente_id, observacao, pedido_produtos } = req.body;
 
+  if (!Array.isArray(pedido_produtos)) {
+    return res
+      .status(400)
+      .json({ mensagem: "O campo 'pedido_produtos' deve ser um array." });
+  }
+
   try {
     const clienteExiste = await knex("clientes")
       .where({ id: cliente_id })
@@ -14,19 +20,18 @@ const cadastrarPedido = async (req, res) => {
     }
 
     const produtosValidos = [];
+
     for (const pedido_produto of pedido_produtos) {
       const { produto_id, quantidade_produto } = pedido_produto;
 
       const produto = await knex("produtos").where({ id: produto_id }).first();
       if (!produto) {
-        return res
-          .status(404)
-          .json({ message: `Produto com ID ${produto_id} não encontrado.` });
+        return res.status(404).json({ mensagem: "Produto não encontrado. " });
       }
 
       if (quantidade_produto > produto.estoque) {
         return res.status(400).json({
-          message: `Quantidade em estoque insuficiente para o produto com ID ${produto_id}.`,
+          mensagem: "Quantidade em estoque insuficiente",
         });
       }
 
@@ -36,17 +41,33 @@ const cadastrarPedido = async (req, res) => {
       });
     }
 
-    await knex("pedidos").insert({
+    // await knex("pedidos").insert({
+    //   cliente_id,
+    //   observacao,
+    //   pedido_produtos,
+    // });
+
+    const pedido = {
       cliente_id,
       observacao,
-      pedido_produtos,
-    });
+      valor_total: 0,
+    };
 
+    const [pedidoId] = await knex("pedidos").insert(pedido);
+
+    // Insira os produtos no pedido
+    for (const produtoValido of produtosValidos) {
+      await knex("pedido_produtos").insert({
+        pedido_id: pedidoId,
+        produto_id: produtoValido.produto_id,
+        quantidade_produto: produtoValido.quantidade_produto,
+      });
+    }
     return res.status(201).json({ mensagem: "Pedido cadastrado com sucesso" });
   } catch (error) {
     return res
       .status(500)
-      .json({ mensagem: "Erro interno do servidor", error: error.message });
+      .json({ mensagem: "Erro interno do servidor", error: error.mensagem });
   }
 };
 
