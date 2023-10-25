230361dd333348d2630178a873c5f2b8218c7327
mar1tomaz <marianatomaz@live.com>
2023-10-25 05:53:45 -0300

    fix: cadastro de produto com imagem

--- src/controlador/produtos/cadastrarProdutos.js
+++ src/controlador/produtos/cadastrarProdutos.js
@@ -1,8 +1,11 @@
 const knex = require("../../conexao");
+const { uploadImagem } = require("../../servicos-upload/uploadImagens");
+
+
 
 const cadastrarProduto = async (req, res) => {
     const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
-
+    const { originalname, mimetype, buffer } = req.file;
     try {
 
         const categoriaExiste = await knex('categorias').where({ id: categoria_id })
@@ -10,13 +13,14 @@ const cadastrarProduto = async (req, res) => {
         if (categoriaExiste.length === 0) {
             return res.status(404).json({ mensagem: 'Categoria não existe' })
         }
-
+        const imagem = await uploadImagem(originalname, mimetype, buffer)
         await knex('produtos').
             insert({
                 descricao,
                 quantidade_estoque,
                 valor,
-                categoria_id
+                categoria_id,
+                produto_imagem: imagem.url
             })
 
         return res.status(201).json({ mensagem: 'Produto cadastrado com sucesso' })
