0e20c442aeeeda1ccb48bdd633a779019236262f
Gleicy Silva <glegleicy5@gmail.com>
2023-10-25 20:24:14 -0300

    Fix: Corrigido validação do Joi

--- src/validacoes/produtoSchema.js
+++ src/validacoes/produtoSchema.js
@@ -1,11 +1,10 @@
 const joi = require('joi')
 
 const produtoSchema = joi.object({
-    descricao: joi.string().trim().required().pattern(/^[^0-9]*$/).messages({
+    descricao: joi.string().trim().required().messages({
         'any.required': 'O campo descrição é obrigatório',
         'string.base': 'O campo descrição deve ser uma string',
         'string.empty': 'O campo descrição é obrigatório',
-        'string.pattern.base': 'O campo descrição não pode conter números',
     }),
     quantidade_estoque: joi.number().required().messages({
         'any.required': 'O campo quantidade em estoque é obrigatório',
