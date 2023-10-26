d862dbad4fa2b0d6c6bf8ee0e334a91be56b187e
Gleicy Silva <glegleicy5@gmail.com>
2023-10-25 22:13:25 -0300

    Fix: Corrigido conflito

--- src/rotas.js
+++ src/rotas.js
@@ -7,13 +7,13 @@ const validarToken = require("./intermediarios/validarToken");
 const atualizarUsuario = require("./controlador/usuarios/atualizarUsuario");
 const detalharUsuario = require("./controlador/usuarios/detalharUsuario");
 const validarRequisicao = require("./intermediarios/validarRequisicao");
-const usuarioSchema = require("./validacoes/usuarios");
+const usuarioSchema = require("./validacoes/usuarioSchema");
 const loginSchema = require("./validacoes/loginSchema");
 const editarCliente = require("./controlador/clientes/editarCliente");
 const detalharCliente = require("./controlador/clientes/detalharCliente");
 const clienteSchema = require("./validacoes/clienteSchema");
 const cadastrarCliente = require("./controlador/clientes/cadastrarClientes");
-const listarClientes = require("./controlador/clientes/listarClientes")
+const listarClientes = require("./controlador/clientes/listarClientes");
 const produtoSchema = require("./validacoes/produtoSchema");
 const cadastrarProduto = require("./controlador/produtos/cadastrarProdutos");
 const listarProdutos = require("./controlador/produtos/listarProdutos")
@@ -26,6 +26,8 @@ const {
   atualizarImagemProduto,
 } = require("./controlador/produtos/produtos");
 const listarPedidos = require("./controlador/listarPedidos");
+const pedidoSchema = require("./validacoes/pedidoSchema");
+const cadastrarPedido = require("./controlador/pedidos/cadastrarPedido");
 
 const rotas = express();
 
@@ -48,8 +50,10 @@ rotas.delete("/produto/:id", excluirProduto);
 
 rotas.post("/cliente", validarRequisicao(clienteSchema), cadastrarCliente);
 rotas.get("/cliente", listarClientes);
-rotas.put('/cliente/:id', validarRequisicao(clienteSchema), editarCliente)
-rotas.get('/cliente/:id', detalharCliente)
+rotas.put("/cliente/:id", validarRequisicao(clienteSchema), editarCliente);
+rotas.get("/cliente/:id", detalharCliente);
+
+rotas.post("/pedido", validarRequisicao(pedidoSchema), cadastrarPedido);
 
 rotas.get("/pedido", listarPedidos)
 
